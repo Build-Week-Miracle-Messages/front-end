11082ff7902bd5f80d1cab827db2e0472ba22b99
Mashima <52222646+zimashima@users.noreply.github.com>
2019-11-21 17:01:32 -0500

    fixed issue with blank connect and place it with NA

--- src/components/caseComponents/CaseList.js
+++ src/components/caseComponents/CaseList.js
@@ -42,10 +42,10 @@ export default function CaseList(props){
                     home_town={client.home_town}
                     current_city={client.current_city}
                     contact={client.contact}
-                    // connect_name={client.connect[0].name}
-                    // connect_age={client.connect[0].age}s
-                    // connect_relationship={client.connect[0].relationship}
-                    // connect_location={client.connect[0].location}
+                    connect_name={client.connect[0].name}
+                    connect_age={client.connect[0].age}s
+                    connect_relationship={client.connect[0].relationship}
+                    connect_location={client.connect[0].location}
                     onDelete={handleDelete}
                     />
                 ))
